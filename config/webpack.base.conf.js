84cd3b30ec6af6b54814bd9e5ee7cde5990a2fd1
Oleg Rudenko <oleg.ru93@yandex.ru>
2019-12-01 23:29:58 +0300

    Создана структура проекта, настроен webpack

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,130 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets" 
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.assets}/js/[name].js`,
+    path: PATHS.dist,
+    // publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      { 
+        test: /\.pug$/, 
+        loader: "pug-loader" 
+      },
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, 
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: "[name].[ext]",
+        },
+        exclude: `${PATHS.src}/fonts`
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      
+    ]
+  },
+  resolve: {
+    alias: {
+      "~": "src"
+    }
+  },
+  
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}/css/[name].css`,
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    // new htmlWebpackPlugin({
+    //   hash: false,
+    //   template: `${PATHS.src}/index.html`,
+    //   filename: "./index.html"
+    // }),
+    new copyWebpackPlugin([
+      { from: `${PATHS.src}/img/`, to: `${PATHS.assets}/img/` },
+      { from: `${PATHS.src}/fonts/`, to: `${PATHS.assets}/fonts/` },
+      { from: `${PATHS.src}/static/`, to: "" },
+    ]),
+
+    // Automatic creation any html pages (Don't forget to RERUN dev server)
+    // see more: https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
+    // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
+    ...PAGES.map(page => new htmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+  ],
+};
