cc9254db2fe4516231fdf0a174e424ee9c13087d
Oleg Rudenko <oleg.ru93@yandex.ru>
2019-12-06 17:10:43 +0300

    Исправлена маска для инпута
    теперь разделитель точка вместо слешей

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,5 +2,5 @@ import $ from 'jquery';
 import "jquery-mask-plugin";
 
 $(document).ready(function(){
-  $('input[name="date"]').mask('00/00/0000');
+  $('input[name="date"]').mask('00.00.0000');
 });
