2040bf161875bc41709528bb341c7c3bf6522b70
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-01-11 00:31:57 +0300

    refactor: improve like-button structure
    like-button now has responsive size
    add jquery script for change gradient border when checked

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,1 +1,15 @@
 import './like-button.scss';
+
+window.$ = window.jQuery = require('jquery');
+
+(function($) {
+
+  const $likeButtonItem = $('.js-like-button__item');
+  const $mod = 'like-button__label_checked';
+
+  $($likeButtonItem).on('change.likeButton', function(){
+    $(this).parent().toggleClass($mod);
+  });
+
+})(jQuery);
+
