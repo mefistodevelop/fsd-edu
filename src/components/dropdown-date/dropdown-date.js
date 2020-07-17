5dc4b1eb427ba9160f9060adb047242a63eb522a
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-07-17 11:08:17 +0300

    feat: add calendar to dropdown-date component

--- src/components/dropdown-date/dropdown-date.js
+++ src/components/dropdown-date/dropdown-date.js
@@ -1,1 +1,39 @@
 import './dropdown-date.scss';
+
+export function findChild(node, child) {
+  return [...node.children].filter((ch) => ch.classList.contains(child))[0];
+}
+
+class DropdownDate {
+  constructor(node) {
+    this.node = node;
+    this.isOpen = false;
+    this.arrivingDate = '';
+    this.departureDate = '';
+    this.container = findChild(this.node, 'dropdown-date__container');
+    this.arrivingDropdown = findChild(this.container, 'dropdown-date__label_arrive');
+    this.departureDropdown = findChild(this.container, 'dropdown-date__label_depart');
+    this.arrivingDropdown.addEventListener('click', this.toggleDropdownVisibility);
+    this.departureDropdown.addEventListener('click', this.toggleDropdownVisibility);
+    this.calendar = findChild(this.node, 'dropdown-date__calendar');
+  }
+
+  setIsOpen = (isOpen) => {
+    this.isOpen = isOpen;
+  }
+
+  toggleDropdownVisibility = () => {
+    if (this.isOpen) {
+      this.calendar.classList.add('dropdown-date__calendar_hidden');
+      this.setIsOpen(false);
+    } else {
+      this.calendar.classList.remove('dropdown-date__calendar_hidden');
+      this.setIsOpen(true);
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdowns = document.getElementsByClassName('dropdown-date');
+  [...dropdowns].forEach((dropdown) => new DropdownDate(dropdown));
+});
